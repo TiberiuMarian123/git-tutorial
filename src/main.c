e80f2c2a57bf0f86a6e32cb4bdd5f121f359fe26
Tiberiu Marian <mtiberiu79@gmail.com>
2020-05-07 01:28:21 +0300

    Add Challenge 2 to main branch

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-
 int main()
 {
-    printf("Hello ESPL\n");
-    return 0;
+   	char x = 'y', result;
+	int val;
+	while(x == 'y')
+		{ 
+			scanf("Please enter a number %d \n", &val);
+			result = num_to_words(val);
+			printf("%c", result);
+			scanf("Would you like to continue (y/n) : %c", x);
+		}
 }
